792ea6f0fc53d19258584e65885f2bec47e1a79f
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-10 23:20:31 +0200

    Already able to lookup blobs

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,42 @@ extern crate indicatif;
 
 use failure::{Error, ResultExt};
 use failure_tools::ok_or_exit;
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::os::unix::ffi::OsStrExt;
+use std::io::{stdin, stdout, BufRead, BufReader};
 
 const PROGRESS_RESOLUTION: usize = 250;
+
+fn recurse_tree(
+    repo: &git2::Repository,
+    commit_oid: &git2::Oid,
+    tree: git2::Tree,
+    lut: &mut BTreeMap<git2::Oid, Vec<git2::Oid>>,
+    path: &mut PathBuf,
+) {
+    use git2::ObjectType::*;
+    for item in tree.iter() {
+        path.push(OsStr::from_bytes(item.name_bytes()));
+        match item.kind() {
+            Some(Tree) => recurse_tree(
+                repo,
+                commit_oid,
+                item.to_object(repo)
+                    .expect("valid object")
+                    .into_tree()
+                    .expect("tree"),
+                lut,
+                path,
+            ),
+            Some(Blob) => lut.entry(item.id())
+                .or_insert_with(|| Vec::new())
+                .push(commit_oid.clone()),
+            _ => continue,
+        }
+    }
+}
 
 fn run() -> Result<(), Error> {
     let repo = git2::Repository::open(std::env::args()
@@ -19,18 +53,37 @@ fn run() -> Result<(), Error> {
     walk.set_sorting(git2::Sort::TOPOLOGICAL);
     walk.push_head()?;
 
+    let mut lut = BTreeMap::<git2::Oid, Vec<git2::Oid>>::new();
     let progress = indicatif::ProgressBar::new_spinner();
     progress.set_draw_target(indicatif::ProgressDrawTarget::stderr());
 
     for oid in walk.filter_map(Result::ok) {
         num_commits += 1;
         if num_commits % PROGRESS_RESOLUTION == 0 {
-            progress.set_message(&format!("Counted {} objects...", num_commits));
+            progress.set_message(&format!("Indexed {} commits...", num_commits));
             progress.tick();
+        }
+        if let Ok(object) = repo.find_object(oid, Some(git2::ObjectType::Commit)) {
+            let commit = object.into_commit().expect("to have commit");
+            let tree = commit.tree().expect("commit to have tree");
+            let mut path = PathBuf::new();
+            recurse_tree(&repo, &oid, tree, &mut lut, &mut path);
         }
     }
     progress.finish_and_clear();
-    eprintln!("READY: Build cache with {} commits", num_commits);
+    eprintln!(
+        "READY: Build cache from {} commits with table of {} blobs",
+        num_commits,
+        lut.len()
+    );
+
+    let stdin = stdin();
+    let read = BufReader::new(stdin.lock());
+    let stdout = stdout();
+
+    for hexsha in read.lines().filter_map(Result::ok) {
+        git2::Oid::from_str(&hexsha)?;
+    }
 
     Ok(())
 }

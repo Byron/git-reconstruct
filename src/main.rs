0342ec5f1013397bd41f3a5e26de661739c63ff2
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-11 09:32:59 +0200

    Simplify code, get rid of unused items

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use std::collections::{BTreeMap, btree_map::Entry};
 use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
 use git2::{Oid, Repository};
 
-const COMMITS_PROGRESS_RESOLUTION: usize = 250;
-
 fn insert_parent_and_has_not_seen_child(
     parent_oid: Oid,
     child_oid: Oid,
@@ -62,35 +60,26 @@ fn recurse_tree(
     refs
 }
 
-fn build_lut(repo: &Repository) -> Result<(BTreeMap<Oid, Vec<Oid>>, Vec<Oid>), Error> {
+fn build_lut(repo: &Repository) -> Result<BTreeMap<Oid, Vec<Oid>>, Error> {
     let mut walk = repo.revwalk()?;
-    let (mut iteration_count, mut total_refs) = (0, 0);
+    let mut total_refs = 0;
     walk.set_sorting(git2::Sort::TOPOLOGICAL);
     walk.push_head()?;
     let mut lut = BTreeMap::new();
-    let mut commits = Vec::new();
+    let mut num_commits = 0;
     let progress = indicatif::ProgressBar::new_spinner();
     progress.set_draw_target(indicatif::ProgressDrawTarget::stderr());
-    for oid in walk.filter_map(Result::ok) {
-        iteration_count += 1;
-        if iteration_count % COMMITS_PROGRESS_RESOLUTION == 0 {
-            progress.set_message(&format!("Traversed {} commits...", iteration_count,));
-            progress.tick();
-        }
-        commits.push(oid);
-    }
-    let num_commits = commits.len();
-    for (cid, commit_oid) in commits.iter().enumerate() {
-        if let Ok(object) = repo.find_object(*commit_oid, Some(git2::ObjectType::Commit)) {
+    for commit_oid in walk.filter_map(Result::ok) {
+        num_commits += 1;
+        if let Ok(object) = repo.find_object(commit_oid, Some(git2::ObjectType::Commit)) {
             let commit = object.into_commit().expect("to have commit");
             let tree = commit.tree().expect("commit to have tree");
-            if insert_parent_and_has_not_seen_child(*commit_oid, tree.id(), &mut lut) {
+            if insert_parent_and_has_not_seen_child(commit_oid, tree.id(), &mut lut) {
                 total_refs += recurse_tree(&repo, tree, &mut lut);
             }
         }
         progress.set_message(&format!(
-            "{}/{} Commits done; Table with {} blobs and a total of {} back-refs",
-            cid,
+            "{} Commits done; reverse-tree with {} entries and a total of {} parent-edges",
             num_commits,
             lut.len(),
             total_refs
@@ -99,18 +88,15 @@ fn build_lut(repo: &Repository) -> Result<(BTreeMap<Oid, Vec<Oid>>, Vec<Oid>), E
     }
     progress.finish_and_clear();
     eprintln!(
-        "READY: Build cache from {} commits with table of {} blobs and {} refs",
+        "READY: Build reverse-tree from {} commits with table of {} entries and {} parent-edges",
         num_commits,
         lut.len(),
         total_refs
     );
-    Ok((lut, commits))
+    Ok(lut)
 }
 
-fn depelete_requests_from_stdin(
-    lut: &mut BTreeMap<Oid, Vec<Oid>>,
-    commits: &mut Vec<Oid>,
-) -> Result<(), Error> {
+fn depelete_requests_from_stdin(lut: &BTreeMap<Oid, Vec<Oid>>) -> Result<(), Error> {
     let stdin = stdin();
     let read = BufReader::new(stdin.lock());
     let stdout = stdout();
@@ -142,8 +128,8 @@ fn run() -> Result<(), Error> {
         .next()
         .ok_or_else(|| format_err!("USAGE: <me> <repository>"))?)?;
 
-    let (mut lut, mut commits) = build_lut(&repo)?;
-    depelete_requests_from_stdin(&mut lut, &mut commits)
+    let lut = build_lut(&repo)?;
+    depelete_requests_from_stdin(&lut)
 }
 
 fn main() {

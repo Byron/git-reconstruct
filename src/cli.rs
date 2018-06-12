5e8393bb92167d3a4b78bce68bed25fdff845f35
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-13 00:09:15 +0200

    Remove everything that is not required, and radically rethink traversal
    If we can keep a lookup table in reverse, we can also keep a forward
    one and use it to... but that would mean a lot of extra work... .
    Traversal of the reverse tree was just too slow for my taste.
    
    Maybe the only way is to multi-thread it... .

--- src/cli.rs
+++ src/cli.rs
@@ -1,65 +1,10 @@
 use failure::Error;
-use lut::{self, MultiReverseCommitGraph};
-use std::io::{stdin, stdout, BufRead, BufReader, Write};
-use git2::Oid;
-use {Options, Stack};
+use lut;
 use find;
-use indicatif::ProgressBar;
-
-const PROGRESS_RATE: usize = 25;
-
-fn deplete_requests_from_stdin(luts: &MultiReverseCommitGraph) -> Result<(), Error> {
-    let all_oids = lut::commit_oids_table(luts);
-    let mut commits = Vec::new();
-
-    let stdin = stdin();
-    let stdout = stdout();
-
-    let read = BufReader::new(stdin.lock());
-    let mut out = stdout.lock();
-    let mut obuf = String::new();
-    let progress = ProgressBar::new_spinner();
-
-    eprintln!("Waiting for input...");
-    let mut total_commits = 0;
-    let mut stack = Stack::default();
-    for (hid, hexsha) in read.lines().filter_map(Result::ok).enumerate() {
-        let oid = Oid::from_str(&hexsha)?;
-
-        lut::commits_by_blob(&oid, luts, &all_oids, &mut stack, &mut commits);
-        total_commits += commits.len();
-
-        obuf.clear();
-        let len = commits.len();
-        for (cid, commit_oid) in commits.iter().enumerate() {
-            use std::fmt::Write;
-            write!(obuf, "{}", commit_oid)?;
-            if cid + 1 < len {
-                obuf.push(' ');
-            }
-        }
-        obuf.push('\n');
-
-        write!(out, "{}", obuf)?;
-        out.flush()?;
-
-        if hid % PROGRESS_RATE == 0 {
-            progress.set_message(&format!(
-                "Looked up {} blobs with a total of {} commits",
-                hid, total_commits
-            ));
-            progress.tick();
-        }
-    }
-    progress.finish_and_clear();
-    Ok(())
-}
+use Options;
 
 pub fn run(opts: Options) -> Result<(), Error> {
-    let tree = opts.tree.clone();
-    let luts = lut::build(opts)?;
-    match tree {
-        None => deplete_requests_from_stdin(&luts),
-        Some(tree) => find::commit(&tree, luts),
-    }
+    let blobs = find::generate_blob_hashes(&opts.tree)?;
+    let _luts = lut::build(&blobs, opts)?;
+    Ok(())
 }

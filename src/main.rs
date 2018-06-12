eb8b8c45379cfd20fe9092cccbb7553ededfc030
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-12 18:13:40 +0200

    Split out lut generation into own module; prepare for merge-generator

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,23 @@ extern crate num_cpus;
 
 use failure::Error;
 use failure_tools::ok_or_exit;
-use std::{mem, collections::{BTreeMap, btree_map::Entry},
-          io::{stdin, stdout, BufRead, BufReader, BufWriter, Write}, path::PathBuf};
-use git2::{ObjectType, Oid, Repository, Revwalk, Tree};
-use indicatif::ProgressBar;
+use std::{collections::BTreeMap, io::{stdin, stdout, BufRead, BufReader, BufWriter, Write},
+          path::PathBuf};
+use git2::{ObjectType, Oid};
 use structopt::StructOpt;
 
-const COMMIT_PROGRESS_RATE: usize = 100;
-const COMPACTION_PROGRESS_RATE: usize = 10000;
+mod lut;
+
+#[derive(Clone)]
+pub enum Capsule {
+    Normal(Vec<Oid>),
+    Compact(Vec<usize>),
+}
 
 /// A basic example
 #[derive(StructOpt, Debug)]
 #[structopt(name = "git-commits-by-blob")]
-struct Options {
+pub struct Options {
     /// The amount of threads to use. If unset, defaults to amount of physical CPUs
     #[structopt(short = "t", long = "threads")]
     threads: Option<usize>,
@@ -40,175 +44,14 @@ struct Options {
     /// the repository to index for queries
     #[structopt(name = "REPOSITORY", parse(from_os_str))]
     repository: PathBuf,
-}
 
-#[derive(Clone)]
-enum Capsule {
-    Normal(Vec<Oid>),
-    Compact(Vec<usize>),
-}
-
-fn insert_parent_and_has_not_seen_child(
-    parent_oid: Oid,
-    child_oid: Oid,
-    lut: &mut BTreeMap<Oid, Capsule>,
-) -> bool {
-    match lut.entry(child_oid) {
-        Entry::Occupied(mut entry) => {
-            if let Capsule::Normal(ref mut parents) = entry.get_mut() {
-                parents.push(parent_oid);
-            }
-            false
-        }
-        Entry::Vacant(entry) => {
-            entry.insert(Capsule::Normal(vec![parent_oid]));
-            true
-        }
-    }
-}
-
-fn recurse_tree(repo: &Repository, tree: Tree, lut: &mut BTreeMap<Oid, Capsule>) -> usize {
-    use ObjectType::*;
-    let mut refs = 0;
-    for item in tree.iter() {
-        match item.kind() {
-            Some(Tree) => {
-                if insert_parent_and_has_not_seen_child(tree.id(), item.id(), lut) {
-                    refs += recurse_tree(
-                        repo,
-                        item.to_object(repo)
-                            .expect("valid object")
-                            .into_tree()
-                            .expect("tree"),
-                        lut,
-                    )
-                }
-            }
-            Some(Blob) => {
-                refs += 1;
-                if let Capsule::Normal(ref mut parents) = lut.entry(item.id())
-                    .or_insert_with(|| Capsule::Normal(Vec::new()))
-                {
-                    parents.push(tree.id());
-                }
-            }
-            _ => continue,
-        }
-    }
-    refs
-}
-
-fn build_lut(opts: Options) -> Result<Vec<BTreeMap<Oid, Capsule>>, Error> {
-    let repo = Repository::open(&opts.repository)?;
-
-    let commits: Vec<_> = {
-        let mut walk = repo.revwalk()?;
-        walk.set_sorting(git2::Sort::TOPOLOGICAL);
-        setup_walk(&repo, &mut walk, opts.head_only)?;
-        walk.filter_map(Result::ok).collect()
-    };
-
-    let multiprogress = indicatif::MultiProgress::new();
-
-    let mut luts: Vec<_> = Vec::new();
-    let num_threads = opts.threads.unwrap_or_else(num_cpus::get_physical);
-    let mut total_refs = 0;
-
-    crossbeam::scope(|scope| {
-        let mut guards = Vec::with_capacity(num_threads);
-        for chunk_of_commits in commits.chunks(commits.len() / num_threads) {
-            let progress = multiprogress.add(ProgressBar::new_spinner());
-            let repo = Repository::open(&opts.repository).unwrap();
-            let no_compact = opts.no_compact;
-            let mut lut = BTreeMap::new();
-
-            let guard = scope.spawn(move || {
-                let (mut num_commits, mut total_refs) = (0, 0);
-                for &commit_oid in chunk_of_commits {
-                    num_commits += 1;
-                    if let Ok(object) = repo.find_object(commit_oid, Some(ObjectType::Commit)) {
-                        let commit = object.into_commit().expect("to have commit");
-                        let tree = commit.tree().expect("commit to have tree");
-                        lut.insert(commit_oid, Capsule::Normal(Vec::new()));
-                        if insert_parent_and_has_not_seen_child(commit_oid, tree.id(), &mut lut) {
-                            total_refs += recurse_tree(&repo, tree, &mut lut);
-                        }
-                    }
-                    if num_commits % COMMIT_PROGRESS_RATE == 0 {
-                        progress.set_message(&format!(
-                                        "{} Commits done; reverse-tree with {} entries and a total of {} parent-edges",
-                                        num_commits,
-                                        lut.len(),
-                                        total_refs
-                                    ));
-                        progress.tick();
-                    }
-                }
-                if !no_compact {
-                    compact_memory(&mut lut, &progress);
-                } else {
-                    eprintln!("INFO: Not compacting memory will safe about 1/3 of used time, at the cost of about 35% more memory")
-                }
-                progress.finish_and_clear();
-                (lut, total_refs)
-            });
-            guards.push(guard);
-        }
-        multiprogress.join_and_clear().unwrap();
-        for guard in guards {
-            let (lut, edges) = guard.join();
-            luts.push(lut);
-            total_refs += edges;
-        }
-    });
-
-    eprintln!(
-        "READY: Build reverse-tree from {} commits with table of {} entries and {} parent-edges",
-        commits.len(),
-        luts.iter().map(|l| l.len()).sum::<usize>(),
-        total_refs
-    );
-    Ok(luts)
-}
-
-fn setup_walk(repo: &Repository, walk: &mut Revwalk, head_only: bool) -> Result<(), Error> {
-    if head_only {
-        walk.push_head()?;
-    } else {
-        let mut refs_pushed = 0;
-        for remote_head in repo.branches(Some(git2::BranchType::Remote))?
-            .filter_map(|b| b.map(|(b, _bt)| b).ok().and_then(|b| b.get().target()))
-        {
-            walk.push(remote_head)?;
-            refs_pushed += 1;
-        }
-        if refs_pushed == 0 {
-            eprintln!("Didn't find a single remote - using head instead to avoid empty traversal");
-            walk.push_head()?;
-        }
-    }
-    Ok(())
-}
-
-fn compact_memory(lut: &mut BTreeMap<Oid, Capsule>, progress: &ProgressBar) -> () {
-    let all_oids: Vec<_> = lut.keys().cloned().collect();
-    for (cid, capsule) in lut.values_mut().enumerate() {
-        let mut compacted = Vec::new();
-        if let Capsule::Normal(ref mut parent_oids) = capsule {
-            compacted = Vec::with_capacity(parent_oids.len());
-            for oid in parent_oids {
-                let parent_idx = all_oids
-                    .binary_search(oid)
-                    .expect("parent to be found in sorted list");
-                compacted.push(parent_idx);
-            }
-        }
-        mem::replace(capsule, Capsule::Compact(compacted));
-        if cid % COMPACTION_PROGRESS_RATE == 0 {
-            progress.set_message(&format!("Compacted {} of {} edges...", cid, all_oids.len(),));
-            progress.tick();
-        }
-    }
+    /// The directory tree for which to figure out the merge commit.
+    /// If unspecified, the program will serve as blob-to-commits lookup table,
+    /// receiving hex-shas of blobs, one per line, on stdin and outputting
+    /// all commits knowing that blob on stdout, separated by space, terminated
+    /// by newline.
+    #[structopt(name = "tree-to-integrate", parse(from_os_str))]
+    tree: Option<PathBuf>,
 }
 
 fn deplete_requests_from_stdin(luts: &Vec<BTreeMap<Oid, Capsule>>) -> Result<(), Error> {
@@ -270,10 +113,11 @@ fn deplete_requests_from_stdin(luts: &Vec<BTreeMap<Oid, Capsule>>) -> Result<(),
 }
 
 fn run(opts: Options) -> Result<(), Error> {
-    let luts = build_lut(opts)?;
+    let luts = lut::build(opts)?;
     deplete_requests_from_stdin(&luts)
 }
 
 fn main() {
-    ok_or_exit(run(Options::from_args()));
+    let opts = Options::from_args();
+    ok_or_exit(run(opts));
 }

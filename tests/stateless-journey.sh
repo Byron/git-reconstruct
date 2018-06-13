1985ef92c75e10f3c39261a22ecfa1b92abfbc06
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-13 09:28:28 +0200

    fix journey tests

--- tests/stateless-journey.sh
+++ tests/stateless-journey.sh
@@ -15,36 +15,20 @@ commit=dc595f7f016a0cff8b176a4c1e67483986f14816
 
 title "backend mode - lookup commits by blob"
 (when "finding the best commit by specifying a source tree"
-  (when "no compaction"
-    it "succeeds" && {
-      WITH_SNAPSHOT="$snapshot/generate-merge-commit-info-success" \
-      expect_run ${SUCCESSFULLY} "$exe" --no-compact --head-only $PWD "$fixture/tree"
-    }
-  )
-  (when "compaction"
-    it "succeeds" && {
-      # WITH_SNAPSHOT="$snapshot/generate-merge-commit-info-success" \
-      expect_run ${SUCCESSFULLY} "$exe" --head-only $PWD "$fixture/tree"
-    }
-  )
+  it "succeeds" && {
+    WITH_SNAPSHOT="$snapshot/generate-merge-commit-info-success" \
+    expect_run ${SUCCESSFULLY} "$exe" --head-only $PWD "$fixture/tree"
+  }
 )
 
 title "backend mode - lookup commits by blob"
 (when "only iterating the current head (--head-only)"
-  (with "memory compaction"
-    it "succeeds" && {
-      echo $commit \
-      | expect_run ${SUCCESSFULLY} "$exe" --head-only $PWD
-    }
-  )
-  (with "no memory compaction (--no-compact)"
-    it "succeeds" && {
-      echo $commit \
-      | expect_run ${SUCCESSFULLY} "$exe" --head-only --no-compact $PWD
-    }
-  )
+  it "succeeds" && {
+    echo $commit \
+    | expect_run ${SUCCESSFULLY} "$exe" --head-only $PWD
+  }
 )
-(when "iterating all remote heads (and memory compaction"
+(when "iterating all remote heads"
   it "succeeds" && {
     echo $commit \
     | expect_run ${SUCCESSFULLY} "$exe" $PWD

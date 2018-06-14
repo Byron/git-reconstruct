0a106ca15a63d88b72c8871c36bfa2ac60f03ab1
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-14 08:54:00 +0200

    Remove explicit drop, more ideomatic

--- src/main.rs
+++ src/main.rs
@@ -109,29 +109,30 @@ mod find {
         let mut commit_indices_to_blobs = vec![FixedBitSet::with_capacity(0); graph.len()];
         let num_threads = num_cpus::get_physical();
         crossbeam::scope(|scope| {
-            let (is, ir) = crossbeam_channel::bounded::<(usize, Oid)>(num_threads);
-            let (os, or) = crossbeam_channel::bounded::<(usize, Vec<usize>)>(num_threads);
-            let blobs = &blobs;
-            scope.spawn(move || {
-                for bid_and_blob in blobs.iter().cloned().enumerate() {
-                    is.send(bid_and_blob);
-                }
-            });
-            for _ in 0..num_threads {
-                let graph = &graph;
-                let ir = ir.clone();
-                let os = os.clone();
+            let or = {
+                let (is, ir) = crossbeam_channel::bounded::<(usize, Oid)>(num_threads);
+                let (os, or) = crossbeam_channel::bounded::<(usize, Vec<usize>)>(num_threads);
+                let blobs = &blobs;
                 scope.spawn(move || {
-                    let mut stack = Stack::default();
-                    for (bid, blob) in ir {
-                        let mut commits = Vec::new();
-                        graph.lookup_idx(&blob, &mut stack, &mut commits);
-                        os.send((bid, commits));
+                    for bid_and_blob in blobs.iter().cloned().enumerate() {
+                        is.send(bid_and_blob);
                     }
                 });
-            }
-            drop(ir);
-            drop(os);
+                for _ in 0..num_threads {
+                    let graph = &graph;
+                    let ir = ir.clone();
+                    let os = os.clone();
+                    scope.spawn(move || {
+                        let mut stack = Stack::default();
+                        for (bid, blob) in ir {
+                            let mut commits = Vec::new();
+                            graph.lookup_idx(&blob, &mut stack, &mut commits);
+                            os.send((bid, commits));
+                        }
+                    });
+                }
+                or
+            };
 
             let mut total_commits = 0;
             for (bid, commits) in or {

b57cf80918b72cbfa9e2380bcc793c6319a5656f
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-15 07:34:41 +0200

    Make journey tests independent of current history

--- tests/stateless-journey.sh
+++ tests/stateless-journey.sh
@@ -14,41 +14,48 @@ SUCCESSFULLY=0
 commit=dc595f7f016a0cff8b176a4c1e67483986f14816
 
 title "Frontend mode - find merge commit"
-(when "finding the best commit by specifying a source tree"
-  (with "cache specified"
-    cache_file=cache.bincode
-    (sandbox
-      it "succeeds" && {
-        expect_run ${SUCCESSFULLY} "$exe" --head-only --cache-path $cache_file "$root/.." "$fixture/tree"
-      }
-      it "writes the cache" && {
-        expect_exists $cache_file
-      }
-      
-      (when "finding the best commit with existing cache" 
-        it "loads the cache and succeeds" && {
+(with "a test repository"
+  cd "$fixture/repo"
+  
+  (when "finding the best commit by specifying a source tree"
+    (with "cache specified"
+      cache_file=cache.bincode
+      (sandbox
+        it "succeeds" && {
+          WITH_SNAPSHOT="$snapshot/generate-merge-commit-info-with-cache-save-success" \
           expect_run ${SUCCESSFULLY} "$exe" --head-only --cache-path $cache_file "$root/.." "$fixture/tree"
         }
+        it "writes the cache" && {
+          expect_exists $cache_file
+        }
+        
+        (when "finding the best commit with existing cache" 
+          it "loads the cache and succeeds" && {
+            WITH_SNAPSHOT="$snapshot/generate-merge-commit-info-with-cache-load-success" \
+            expect_run ${SUCCESSFULLY} "$exe" --head-only --cache-path $cache_file "$root/.." "$fixture/tree"
+          }
+        )
       )
     )
+    (with "no cache specified"
+      it "succeeds" && {
+        WITH_SNAPSHOT="$snapshot/generate-merge-commit-info-success" \
+        expect_run ${SUCCESSFULLY} "$exe" --head-only "$PWD" "$fixture/tree"
+      }
+    )
   )
-  (with "no cache specified"
+
+  title "backend mode - lookup commits by blob"
+  (when "only iterating the current head (--head-only)"
     it "succeeds" && {
-      expect_run ${SUCCESSFULLY} "$exe" --head-only "$PWD" "$fixture/tree"
+      echo $commit \
+      | expect_run ${SUCCESSFULLY} "$exe" --head-only "$PWD"
     }
   )
-)
-
-title "backend mode - lookup commits by blob"
-(when "only iterating the current head (--head-only)"
-  it "succeeds" && {
-    echo $commit \
-    | expect_run ${SUCCESSFULLY} "$exe" --head-only $PWD
-  }
-)
-(when "iterating all remote heads"
-  it "succeeds" && {
-    echo $commit \
-    | expect_run ${SUCCESSFULLY} "$exe" $PWD
-  }
+  (when "iterating all remote heads"
+    it "succeeds" && {
+      echo $commit \
+      | expect_run ${SUCCESSFULLY} "$exe" "$PWD"
+    }
+  )
 )
